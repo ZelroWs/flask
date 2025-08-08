2fe9ea7040606e9c62e7191a38e1f9bcd28c1383
ZelroWs <zelrowspower14@gmail.com>
2025-08-08 17:13:00 -0300

    terceiro commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databse.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'CHAVE_ULTRA_MEGA_HIPER_SECRETA'
 
+db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
 from app.views import homepage
+from app.models import Contato
